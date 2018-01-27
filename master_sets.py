bdb73e9f738b762707e0088b93ef1ab24384f10a
brokerstir <take1risk@gmail.com>
2018-01-26 20:34:38 -0500

    initial commmit

---
+++ master_sets.py
@@ -0,0 +1,10 @@
+# Python practice part 4
+# Master sets for Data Science with Python 3
+
+# Set is a type of list where only unique values are stored
+
+# Create list with duplicates
+nums = [1, 2, 1, 1, 3, 4]
+# Convert to a sets
+unique_nums = set(nums)
+print(unique_nums)
