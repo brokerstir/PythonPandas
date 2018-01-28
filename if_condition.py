c5a2663db2c5501842d1aede457676e4d967a667
brokerstir <take1risk@gmail.com>
2018-01-28 06:54:52 -0500

    initial commit

---
+++ if_condition.py
@@ -0,0 +1,15 @@
+# If is a conditional statement
+
+# If tomorrow is warm, I will wear a T-shirt
+
+# if condition:
+    # do something
+
+tomorrow_temp = 25
+my_clothes = 'jacket' # default
+warm_criterion = 20
+
+if tomorrow_temp > warm_criterion:
+    my_clothes = 'T-shirt'
+
+print("Tomorrow, I will wear a " + my_clothes)
