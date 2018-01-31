bc4d30e8c39e5736222c1239230a227e83675c22
brokerstir <take1risk@gmail.com>
2018-01-31 04:15:32 -0500

    export data commands

--- import_data.py
+++ import_data.py
@@ -14,3 +14,14 @@ print(data_excel)
 # Speciy another sheetname as parameter
 data_excel = pd.read_excel('customers.xlsx', sheetname = 'Sheet2')
 print(data_excel)
+
+# It's also easy to export data
+data.to_csv('customers_out.csv')
+
+# Note panda will add an index to exported data
+#This can be turned off
+data.to_csv('customers_out.csv', index = False)
+
+# Exporting to excel is similar
+data.to_excel('customers_out.xlsx')
+data.to_excel('customers_out.xlsx', index = False)
