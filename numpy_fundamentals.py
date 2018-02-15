28d2584347274010ad26fe224482f6a86a5d4800
brokerstir <take1risk@gmail.com>
2018-02-15 00:30:22 -0500

    How to access specific stats

---
+++ numpy_fundamentals.py
@@ -0,0 +1,13 @@
+import numpy as np
+# create array with 3 rows, 5 cols and print
+a = np.arange(15).reshape(3,5)
+print(a)
+
+# Get number of dimensions for array
+a.ndim
+
+# Get shape of array
+a.shape
+
+# Get number of elements in array
+n.size
