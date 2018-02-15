0ff26d18a7ebd1fed77bcfa588d653dcef229afb
brokerstir <take1risk@gmail.com>
2018-02-15 03:05:16 -0500

    initial commit

---
+++ array_ops.py
@@ -0,0 +1,16 @@
+import numpy as np
+a = np.array([2,3,5,7,11])
+a-10
+# Arithmetic operations on arrays create new arrays
+
+# Multiply each element in array
+a*2
+
+# Exponentiate
+a**2
+
+# Trig operations
+np.cos(a)
+
+# Boolean
+a<6
